a31156297ffff565088364450f539ca949e04a78
DFalque <falque@me.com>
2021-07-30 21:20:10 +0200

    prototype

--- src/screens/main/EventCar.js
+++ src/screens/main/EventCar.js
@@ -1,15 +1,68 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, FlatList, TouchableOpacity, StyleSheet} from 'react-native';
 
-const EventCar = () => {
+const EventCar = ({route, navigation}) => {
+  const {params} = route;
+  const {handleStatus} = params;
+
+  const changeCar = e => {
+    handleStatus(e);
+    navigation.navigate('InfoCar');
+  };
   return (
     <View>
-      <Text>Es revisión</Text>
-      <Text>Pidiendo Piezas</Text>
-      <Text>Arreglando</Text>
-      <Text>Preprando</Text>
+      <Text
+        style={{
+          alignSelf: 'center',
+          marginTop: 20,
+          marginBottom: 20,
+          fontSize: 28,
+          lineHeight: 33,
+        }}>
+        Reparación
+      </Text>
+      <View style={styles.line}></View>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => changeCar('observation')}>
+        <Text>Es revisión</Text>
+      </TouchableOpacity>
+      <View style={styles.line}></View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => changeCar('pieces')}>
+        <Text>Pidiendo Piezas</Text>
+      </TouchableOpacity>
+      <View style={styles.line}></View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => changeCar('repairing')}>
+        <Text>Reparando</Text>
+      </TouchableOpacity>
+      <View style={styles.line}></View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => changeCar('preparing')}>
+        <Text>Preparando </Text>
+      </TouchableOpacity>
+      <View style={styles.line}></View>
+
+      <TouchableOpacity
+        style={styles.button}
+        onPress={() => changeCar('ready')}>
+        <Text>Listo</Text>
+      </TouchableOpacity>
+      <View style={styles.line}></View>
     </View>
   );
 };
 
 export default EventCar;
+
+const styles = StyleSheet.create({
+  button: {width: '100%', margin: 20},
+  line: {height: 0.8, backgroundColor: 'grey'},
+});
--- src/screens/main/InfoCar.js
+++ src/screens/main/InfoCar.js
@@ -25,10 +25,16 @@ const InfoCar = ({route, navigation}) => {
   console.log(route.params);
   const [carInfo, setCarInfo] = useState({});
   const [idCar, setIdCar] = useState('');
-  const [repairing, setRepairing] = useState(false);
+  const [status, setStatus] = useState();
 
   const user = route.params.user;
   const img = require('../../img/audi.jpg');
+  const imgInfo = require('../../img/infoButton.png');
+  const imgHistorial = require('../../img/historialIcon.png');
+  const imgRepair = require('../../img/repairIcon.png');
+  const imgChat = require('../../img/chatIcon.png');
+  const imgShop = require('../../img/workshopIcon.png');
+  const imgBill = require('../../img/billIcon.png');
 
   useEffect(() => {
     console.log('Este es el usuario');
@@ -43,37 +49,51 @@ const InfoCar = ({route, navigation}) => {
         setIdCar(doc._docs[0].id);
         const car = doc._docs[0].data();
         setCarInfo(car);
+        setStatus(car.status);
       });
   }, []);
 
-  const handleEventCar = () => {
-    navigation.navigate('EventCar');
+  const statusCar = {
+    ready: 'Listo',
+    repairing: 'El coche está en reparación',
+    observation: 'Inspeccionando',
+    pieces: 'Pidiendo piezas',
+    preparing: 'Preparando la entrega',
   };
 
-  const changeStatus = () => {
-    if (!repairing) {
-      firestore()
-        .collection('Cars')
-        .doc(idCar)
-        .update({
-          status: 'Repairing',
-        })
-        .then(() => {
-          console.log('Car is repairing!');
-          setRepairing(!repairing);
-        });
-    } else {
-      firestore()
-        .collection('Cars')
-        .doc(idCar)
-        .update({
-          status: 'Ready',
-        })
-        .then(() => {
-          console.log('Car Ready!');
-          setRepairing(!repairing);
-        });
-    }
+  const handleStatus = e => {
+    setStatus(e);
+    changeStatus(e);
+    console.log(status);
+  };
+
+  const handleEventCar = () => {
+    navigation.navigate('EventCar', {
+      handleStatus: handleStatus,
+    });
+  };
+
+  const changeStatus = e => {
+    firestore()
+      .collection('Cars')
+      .doc(idCar)
+      .update({
+        status: e,
+      })
+      .then(() => {
+        setStatus(e);
+      });
+  };
+
+  const noHaceNada = () => {
+    console.log('No hace nada');
+  };
+
+  const renderBarText = () => {
+    console.log(carInfo.status);
+    console.log(statusCar[status]);
+
+    return statusCar[status];
   };
 
   return (
@@ -84,32 +104,109 @@ const InfoCar = ({route, navigation}) => {
       </View>
       <View
         style={{width: '80%', alignSelf: 'center', justifyContent: 'center'}}>
-        {repairing ? (
+        {status === 'ready' ? (
           <Progress.Bar
             style={{alignSelf: 'center', margin: 10}}
-            progress={0.3}
-            width={300}
-            color={'red'}
+            color="green"
+            progress={1}
+            width={200}
+          />
+        ) : status === 'repairing' ? (
+          <Progress.Bar
+            style={{alignSelf: 'center', margin: 10}}
+            color="blue"
+            progress={0.6}
+            width={200}
+          />
+        ) : status === 'pieces' ? (
+          <Progress.Bar
+            style={{alignSelf: 'center', margin: 10}}
+            color="orange"
+            progress={0.4}
+            width={200}
+          />
+        ) : status === 'observation' ? (
+          <Progress.Bar
+            style={{alignSelf: 'center', margin: 10}}
+            color="red"
+            progress={0.2}
+            width={200}
+          />
+        ) : status === 'preparing' ? (
+          <Progress.Bar
+            style={{alignSelf: 'center', margin: 10}}
+            color="blue"
+            progress={0.9}
+            width={200}
           />
         ) : (
-          <Progress.Bar
-            style={{alignSelf: 'center', margin: 10}}
-            progress={1}
-            width={300}
-            color={'green'}
-          />
+          <Text>Vacio</Text>
         )}
+        <Text style={{alignSelf: 'center'}}>{renderBarText()}</Text>
       </View>
-      <TouchableOpacity style={styles.button} onPress={() => changeStatus()}>
-        <Text style={{color: 'white'}}>
-          {repairing ? 'Parar reparación' : 'Iniciar Reparación'}{' '}
-        </Text>
-      </TouchableOpacity>
-      <TouchableOpacity
-        style={styles.buttonSecondary}
-        onPress={() => handleEventCar()}>
-        <Text style={{color: 'black'}}>Añadir Evento</Text>
-      </TouchableOpacity>
+      <View style={styles.containerButton}>
+        <TouchableOpacity
+          style={styles.buttonSecondary2}
+          onPress={() => noHaceNada()}>
+          <View style={styles.icon}>
+            <ImageBackground
+              source={imgInfo}
+              style={styles.image}></ImageBackground>
+          </View>
+          <Text style={{color: 'black'}}>Info</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonSecondary2}
+          onPress={() => noHaceNada()}>
+          <View style={styles.icon}>
+            <ImageBackground
+              source={imgHistorial}
+              style={styles.image}></ImageBackground>
+          </View>
+          <Text style={{color: 'black'}}>Historial</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonSecondary2}
+          onPress={() => handleEventCar()}>
+          <View style={styles.icon}>
+            <ImageBackground
+              source={imgRepair}
+              style={styles.image}></ImageBackground>
+          </View>
+          <Text style={{color: 'black'}}>Reparación</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonSecondary2}
+          onPress={() => noHaceNada()}>
+          <View style={styles.icon}>
+            <ImageBackground
+              source={imgChat}
+              style={styles.image}></ImageBackground>
+          </View>
+          <Text style={{color: 'black'}}>Chat</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style={styles.buttonSecondary2}
+          onPress={() => noHaceNada()}>
+          <View style={styles.icon}>
+            <ImageBackground
+              source={imgShop}
+              style={styles.image}></ImageBackground>
+          </View>
+          <Text style={{color: 'black'}}>Talleres</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.buttonSecondary2}
+          onPress={() => noHaceNada()}>
+          <View style={styles.icon}>
+            <ImageBackground
+              source={imgBill}
+              style={styles.image}></ImageBackground>
+          </View>
+          <Text style={{color: 'black'}}>Facturas</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -170,5 +267,33 @@ const styles = StyleSheet.create({
     borderStyle: 'solid',
     borderBottomColor: 'black',
     borderWidth: 2,
+  },
+
+  containerButton: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignSelf: 'center',
+    //backgroundColor: 'red',
+    width: '70%',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  buttonSecondary2: {
+    justifyContent: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    width: 80,
+    marginRight: 20,
+    marginLeft: 20,
+    height: 70,
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  icon: {
+    width: 70,
+    height: 60,
+    borderRadius: 10,
   },
 });
